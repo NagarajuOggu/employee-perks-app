2e4289f1e173d400f1819444c49e2d81699d8063
Nagaraju Oggu <nagaraju.oggu@gspann.com>
2023-11-13 21:44:51 +0530

    Client template code

---
+++ client/src/components/Container.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Container() {
+    return (
+      <div className="App-Container">
+        <h1> Hello World</h1>
+      </div>
+    );
+  }
+  
+  export default Container;
