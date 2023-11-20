cc5ba689ebd3cc526983877d7755d8b7ab6029a3
Kappa Praveen <praveennkappa@gamil.com>
2023-11-20 18:57:41 +0530

    Updated styles for address form

--- client/src/components/Address-form.tsx
+++ client/src/components/Address-form.tsx
@@ -72,7 +72,7 @@ function AddressForm(props: any) {
   ]);
 
   let validateField = (name: string, value: string | number) => {
-    let errorMsg = '';
+    let errorMsg = "";
     switch (name) {
       case "country":
         if (!value) errorMsg = "Please select Country.";
@@ -146,16 +146,16 @@ function AddressForm(props: any) {
 
   return (
     <>
-      <div className="w-full  mx-auto p-4">
+      <div className="w-full  mx-auto p-4 text-sm">
         <div>
-          <div className="flex items-center w-full py-3">
-            <label className="block w-1/2 text-center" htmlFor="country">
-              Country
+          <div className="flex items-center w-full py-1">
+            <label className="block w-1/2 text-right me-3" htmlFor="country">
+              Country:
             </label>
             <div className="w-1/2 text-left">
               <select
                 name="country"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 w-9/12 px-2 rounded border-slate-400 ${
                   formErrors.country ? "border-red-500" : ""
                 } `}
                 onChange={handleChange}
@@ -169,20 +169,20 @@ function AddressForm(props: any) {
                 ))}
               </select>
               {formErrors.country && (
-                <span className="text-xs text-red-500">
+                <span className="text-xs block text-red-500">
                   {formErrors.country}
                 </span>
               )}
             </div>
           </div>
-          <div className="flex items-center w-full py-3">
-            <label className="block w-1/2 text-center" htmlFor="firstName">
-              First Name
+          <div className="flex items-center w-full py-1">
+            <label className="block w-1/2 text-right me-3" htmlFor="firstName">
+              First Name:
             </label>
             <div className=" text-left w-1/2">
               <input
                 name="firstName"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 w-9/12 rounded px-2 border-slate-400 ${
                   formErrors.firstName ? "border-red-500" : ""
                 } `}
                 value={addressForm.firstName}
@@ -191,20 +191,20 @@ function AddressForm(props: any) {
                 onBlur={handleChange}
               />
               {formErrors.firstName && (
-                <span className="text-xs text-red-500">
+                <span className="text-xs block text-red-500">
                   {formErrors.firstName}
                 </span>
               )}
             </div>
           </div>
-          <div className="flex items-center w-full py-3">
-            <label className="block w-1/2 text-center" htmlFor="lastName">
-              Last Name
+          <div className="flex items-center w-full py-1">
+            <label className="block w-1/2  text-right me-3" htmlFor="lastName">
+              Last Name:
             </label>
             <div className="text-left w-1/2">
               <input
                 name="lastName"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 w-9/12 rounded px-2 border-slate-400 ${
                   formErrors.lastName ? "border-red-500" : ""
                 } `}
                 value={addressForm.lastName}
@@ -213,49 +213,49 @@ function AddressForm(props: any) {
                 onBlur={handleChange}
               />
               {formErrors.lastName && (
-                <span className="text-xs text-red-500">
+                <span className="text-xs block text-red-500">
                   {formErrors.lastName}
                 </span>
               )}
             </div>
           </div>
-          <div className="flex items-center w-full pt-3 pb-1">
-            <label className="block w-1/2 text-center" htmlFor="address">
-              Address
+          <div className="flex items-center w-full pt-1 pb-1">
+            <label className="block w-1/2 text-right me-3" htmlFor="address">
+              Address:
             </label>
             <div className="text-left w-1/2">
               <textarea
                 name="address"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 rounded w-9/12 px-2 border-slate-400 ${
                   formErrors.address ? "border-red-500" : ""
                 } `}
                 onChange={handleChange}
                 onBlur={handleChange}
               ></textarea>
               {formErrors.address && (
-                <span className="text-xs text-red-500">
+                <span className="text-xs block text-red-500">
                   {formErrors.address}
                 </span>
               )}
             </div>
           </div>
-          <div className="flex items-center w-full pb-3 pt-1">
+          <div className="flex items-center w-full pb-1 pt-1">
             <label
-              className="block w-1/2 text-center"
+              className="block w-1/2 text-right me-3"
               htmlFor="addressSecondLine"
             >
-              Address second line
+              Address second line:
             </label>
             <div className=" w-1/2">
               <textarea
                 name="addressSecondLine"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 rounded w-9/12 px-2 border-slate-400 ${
                   formErrors.addressSecondLine ? "border-red-500" : ""
                 } `}
                 onChange={handleChange}
                 onBlur={handleChange}
               ></textarea>
-              <span className="text-xs">
+              <span className="text-xs block">
                 ex: ATP#, PO Box, Leave Blank if none
               </span>
               {formErrors.addressSecondLine && (
@@ -265,14 +265,14 @@ function AddressForm(props: any) {
               )}
             </div>
           </div>
-          <div className="flex items-center w-full py-3">
-            <label className="block w-1/2 text-center" htmlFor="phone">
+          <div className="flex items-center w-full py-1">
+            <label className="block w-1/2 text-right me-3" htmlFor="phone">
               Phone#
             </label>
             <div className="text-left w-1/2">
               <input
                 name="phone"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 rounded w-9/12 px-2 border-slate-400 ${
                   formErrors.phone ? "border-red-500" : ""
                 } `}
                 type="text"
@@ -280,18 +280,20 @@ function AddressForm(props: any) {
                 onBlur={handleChange}
               />
               {formErrors.phone && (
-                <span className="text-xs text-red-500">{formErrors.phone}</span>
+                <span className="text-xs block text-red-500">
+                  {formErrors.phone}
+                </span>
               )}
             </div>
           </div>
-          <div className="flex items-center w-full py-3">
-            <label className="block w-1/2 text-center" htmlFor="city">
-              City
+          <div className="flex items-center w-full py-1">
+            <label className="block w-1/2 text-right me-3" htmlFor="city">
+              City:
             </label>
             <div className="text-left w-1/2">
               <input
                 name="city"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 rounded w-9/12 px-2 border-slate-400 ${
                   formErrors.city ? "border-red-500" : ""
                 } `}
                 type="text"
@@ -299,19 +301,21 @@ function AddressForm(props: any) {
                 onBlur={handleChange}
               />
               {formErrors.city && (
-                <span className="text-xs text-red-500">{formErrors.city}</span>
+                <span className="text-xs block text-red-500">
+                  {formErrors.city}
+                </span>
               )}
             </div>
           </div>
-          <div className="flex items-center w-full py-3">
-            <label className="block w-1/2 text-center" htmlFor="stateCode">
-              State code
+          <div className="flex items-center w-full py-1">
+            <label className="block w-1/2 text-right me-3" htmlFor="stateCode">
+              State code:
             </label>
             <div className="text-left w-1/2">
               <select
                 name="stateCode"
                 id="stateCode"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 rounded w-9/12 px-2 border-slate-400 ${
                   formErrors.stateCode ? "border-red-500" : ""
                 } `}
                 onChange={handleChange}
@@ -325,20 +329,20 @@ function AddressForm(props: any) {
                 ))}
               </select>
               {formErrors.stateCode && (
-                <span className="text-xs text-red-500">
+                <span className="text-xs block text-red-500">
                   {formErrors.stateCode}
                 </span>
               )}
             </div>
           </div>
-          <div className="flex items-center  w-full py-3">
-            <label className="block w-1/2 text-center" htmlFor="zipCode">
+          <div className="flex items-center  w-full py-1">
+            <label className="block w-1/2 text-right me-3" htmlFor="zipCode">
               Zip Code
             </label>
             <div className="text-left w-1/2">
               <input
                 name="zipCode"
-                className={` border-2 w-full px-2 py-1 border-slate-400 ${
+                className={` border-2 rounded w-9/12 px-2 border-slate-400 ${
                   formErrors.zipCode ? "border-red-500" : ""
                 } `}
                 type="text"
@@ -346,16 +350,11 @@ function AddressForm(props: any) {
                 onBlur={handleChange}
               />
               {formErrors.zipCode && (
-                <span className="text-xs text-red-500">
+                <span className="text-xs block text-red-500">
                   {formErrors.zipCode}
                 </span>
               )}
             </div>
-          </div>
-          <div className="flex items-center justify-center py-3 w-full">
-            <button className="p-2 bg-sky-500" onClick={formSubmit}>
-              Submit
-            </button>
           </div>
         </div>
       </div>
--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -2,31 +2,33 @@ import Instructions from "./Instructions";
 import Products from "./products";
 import AddressForm from "./Address-form";
 import { EmployeePerks } from "../types/app-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Container(props: EmployeePerks) {
   const [pointsPerMonth, setMonthlyPoints] = useState(0);
-  const [monthSelected, setMonthSelected] = useState('');
+  const [monthSelected, setMonthSelected] = useState("january");
   const [seat, setSeat] = useState(0);
-  const [ shippingLocation, setShippingLocation] = useState('none');
+  const [shippingLocation, setShippingLocation] = useState("none");
 
   const currentDate = new Date().toLocaleDateString();
-  const {title, logo, user} = props;
+  const { title, logo, user } = props;
 
   const handleMonthSelection = (event) => {
     event.stopPropagation();
     setMonthSelected(event.target.value);
     setMonthlyPoints(user.perks[event.target.value].availablePoints);
-  }
+  };
+
+  useEffect(() => {
+    setMonthlyPoints(user.perks[monthSelected].availablePoints);
+  }, []);
 
   return (
     <div className="container mx-auto  border">
       <div className="flex flex-col items-center mt-8">
         <div className="text-center">
           <img src={logo} alt="Logo" className="w-16 h-16 mb-2" />
-          <p className="text-lg font-bold text-blue-500">
-            {title}
-          </p>
+          <p className="text-lg font-bold text-blue-500">{title}</p>
         </div>
 
         <div className="mr-2 text-sm text-gray-600 self-end">{currentDate}</div>
@@ -52,14 +54,18 @@ function Container(props: EmployeePerks) {
                     <select
                       id="month"
                       name="month"
+                      defaultValue={"january"}
                       className="border rounded-md w-full ml-1"
-                      value={monthSelected} onChange={ handleMonthSelection}
-                    > 
-                      {
-                        Object.keys(user.perks).map( (key) => {
-                          return <option value={key} key={key}>{key}</option>
-                        })
-                      }
+                      value={monthSelected}
+                      onChange={handleMonthSelection}
+                    >
+                      {Object.keys(user.perks).map((key) => {
+                        return (
+                          <option value={key} key={key}>
+                            {key}
+                          </option>
+                        );
+                      })}
                     </select>
                     <p className="text-sm text-gray-500 mt-1 text-right mr-auto ml-1">
                       Select a month to see available points
@@ -77,7 +83,9 @@ function Container(props: EmployeePerks) {
                     Full Name :
                   </label>
                   <div className="flex flex-col ml-2">
-                    <p id="fullName">{user.firstName} {user.lastName} - {user.role}</p>
+                    <p id="fullName">
+                      {user.firstName} {user.lastName} - {user.role}
+                    </p>
                     <div className="flex flex-row justify-center items-center">
                       <input
                         type="checkbox"
@@ -103,7 +111,8 @@ function Container(props: EmployeePerks) {
                     My EPP Points:
                   </p>
                   <p className="text-xs w-1/2 ml-1">
-                    <span className="font-bold px-1">{pointsPerMonth}</span> / <span>{user.pointsPerMonth}</span>
+                    <span className="font-bold px-1">{pointsPerMonth}</span> /{" "}
+                    <span>{user.pointsPerMonth}</span>
                   </p>
                 </div>
               </td>
@@ -119,7 +128,7 @@ function Container(props: EmployeePerks) {
                     <select
                       className="border rounded-md w-3/4 text-sm ml-1"
                       id="location"
-                      onChange={ (e) => setShippingLocation(e.target.value)}
+                      onChange={(e) => setShippingLocation(e.target.value)}
                     >
                       <option value="none"> </option>
                       <option value="ship-to-home"> SHIP TO HOME</option>
@@ -137,9 +146,14 @@ function Container(props: EmployeePerks) {
 
             <tr>
               <td className="pt-2 border min-h-fit w-1/2">
-
-                
-                {shippingLocation !== 'none' ? <AddressForm firstName= {user.firstName} lastName= {user.lastName} /> : <div className="h-80"></div>}
+                {shippingLocation !== "none" ? (
+                  <AddressForm
+                    firstName={user.firstName}
+                    lastName={user.lastName}
+                  />
+                ) : (
+                  <div className="h-80"></div>
+                )}
               </td>
               <td className="pt-2 border w-1/2 align-top">
                 <div className="flex flex-row justify-center mb-1">
